6e2d7931544897cbde8f1e3a55ede620e4f8e8a2
Lili Sazonova <lilissazonova@gmail.com>
2024-08-20 16:53:30 +0300

    add generic 4.ts

---
+++ src/generics/4.ts
@@ -0,0 +1,24 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<User>): User {
+  const defaultUser: User = {
+    name: 'userName',
+    surname: 'userSurname',
+    email: '',
+    password: '',
+  };
+    
+  return { ...defaultUser, ...initialValues };
+}
+
+const UpdateUser = createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
+
+console.log(UpdateUser);
