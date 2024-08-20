5e9a5f52981957a31022a38d23328b1030f616af
Lili Sazonova <lilissazonova@gmail.com>
2024-08-20 16:34:54 +0300

    add generic 2.ts

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,15 +1,3 @@
-// Завдання 1
-
-// Виконуйте це завдання у файлі src/generics/1.ts.
-
-
-
-// Типізуйте асинхронну функцію fetchData, 
-// яка приймає URL ресурсу та повертає об'єкт з даними. 
-// Використовуйте Generics для типізації повернутих даних.
-
-
-
 import axios from 'axios';
 
 async function fetchData<T>(url:string):Promise<T> {
